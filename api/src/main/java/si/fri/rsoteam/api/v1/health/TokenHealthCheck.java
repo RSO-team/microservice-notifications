303d79fd174858c04c9c76f72b05f68b851bf46f
Luka Galjot <luka.galjot@gmail.com>
2022-01-03 23:40:46 +0100

    Add custom health check

---
+++ api/src/main/java/si/fri/rsoteam/api/v1/health/TokenHealthCheck.java
@@ -0,0 +1,26 @@
+package si.fri.rsoteam.api.v1.health;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+import si.fri.rsoteam.services.config.RestConfig;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@Liveness
+@ApplicationScoped
+public class TokenHealthCheck implements HealthCheck {
+
+    @Inject
+    private RestConfig restProperties;
+
+    @Override
+    public HealthCheckResponse call() {
+        if (restProperties.getD7Token() != null) {
+            return HealthCheckResponse.down(MaintenanceHealthCheck.class.getSimpleName());
+        } else {
+            return HealthCheckResponse.up(MaintenanceHealthCheck.class.getSimpleName());
+        }
+    }
+}
